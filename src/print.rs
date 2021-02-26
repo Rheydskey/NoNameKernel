4feccacc854558cfdbb537bc9438b77caacbf131
Rheydskey <matgag02@gmail.com>
2021-02-26 19:17:33 +0100

    ✨ Add print_color and print_bytes

--- src/print.rs
+++ src/print.rs
@@ -1,5 +1,14 @@
 use crate::lib::vga::Writer;
+use crate::lib::vga_color::{ColorCode, Color};
 
 pub fn print(buf: &mut Writer, msg: &str) {
+    buf.color_code = ColorCode::new(Color::White, Color::Black);
+    buf.write_string(msg);
+}
+pub fn print_bytes(buf: &mut Writer, msg: u8) {
+    buf.write_byte(msg)
+}
+pub fn print_color(buf: &mut Writer, msg: &str, color: ColorCode) {
+    buf.color_code = color;
     buf.write_string(msg);
 }

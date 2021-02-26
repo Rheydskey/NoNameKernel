29827ee6657d83199cdd13ca0f34a04dc87eca05
Rheydskey <matgag02@gmail.com>
2021-02-26 23:38:21 +0100

    🚚 Move Panic Handler to panic.rs

--- src/main.rs
+++ src/main.rs
@@ -3,45 +3,15 @@
 #![feature(asm)]
 #![feature(panic_info_message)]
 
-mod arch;
+
 use crate::arch::x86_64::gdt::gdt_install;
 use crate::lib::vga::Writer;
 use crate::lib::vga_color::{Color, ColorCode};
-use core::panic::PanicInfo;
-mod utils;
-mod lib;
-
 use core::fmt::Write;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    let mut buffer = Writer::default();
-    let panic_message = match _info.message() {
-        Some(arg) => arg.as_str().unwrap_or("No Message Error"),
-        None => "No message Error",
-    };
-    let location = match _info.location() {
-        Some(e) => e,
-        None => {
-            buffer.color_code = ColorCode::new(Color::Red, Color::Black);
-            write!(buffer, "No Panic Location => {}", panic_message).unwrap();
-            buffer.new_line();
-            loop {};
-        }
-    };
-    let panic_file = location.file();
-    let panic_column = location.column();
-    let panic_line = location.line();
-    buffer.color_code = ColorCode::new(Color::Red, Color::Black);
-    write!(
-        buffer,
-        "PANIC at {}:{}:{} => {}",
-        panic_file, panic_line, panic_column, panic_message
-    )
-    .unwrap();
-    buffer.new_line();
-    loop {}
-}
+mod arch;
+mod utils;
+mod lib;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
---
+++ src/utils/panic.rs
@@ -0,0 +1,34 @@
+use core::panic::PanicInfo;
+use crate::lib::vga::Writer;
+use crate::lib::vga_color::{Color, ColorCode};
+use core::fmt::Write;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    let mut buffer = Writer::default();
+    let panic_message = match _info.message() {
+        Some(arg) => arg.as_str().unwrap_or("No Message Error"),
+        None => "No message Error",
+    };
+    let location = match _info.location() {
+        Some(e) => e,
+        None => {
+            buffer.color_code = ColorCode::new(Color::Red, Color::Black);
+            write!(buffer, "No Panic Location => {}", panic_message).unwrap();
+            buffer.new_line();
+            loop {};
+        }
+    };
+    let panic_file = location.file();
+    let panic_column = location.column();
+    let panic_line = location.line();
+    buffer.color_code = ColorCode::new(Color::Red, Color::Black);
+    write!(
+        buffer,
+        "PANIC at {}:{}:{} => {}",
+        panic_file, panic_line, panic_column, panic_message
+    )
+    .unwrap();
+    buffer.new_line();
+    loop {}
+}

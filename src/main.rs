aeadf81a065faf499b186d39455fb5b280a0bc1e
Rheydskey <matgag02@gmail.com>
2021-02-26 19:16:59 +0100

    ♻️ Rector start message

--- src/main.rs
+++ src/main.rs
@@ -32,16 +32,9 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    let hello = b"Welcome on NoName Kernel";
-    for (i, &byte) in hello.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
     let mut buffer = Writer::default();
+    print_color(&mut buffer, "Welcome on NoName Kernel", ColorCode::new(Color::LightCyan, Color::Black));
+    buffer.new_line();
     unsafe { gdt_install(&mut buffer) };
     loop {}
 }

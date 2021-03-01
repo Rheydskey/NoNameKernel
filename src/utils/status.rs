29086a8a61a80f917d42ce640009fb26d95c2f12
Rheydskey <matgag02@gmail.com>
2021-03-01 13:50:56 +0100

    :sparkles: Add a Status function

---
+++ src/utils/status.rs
@@ -0,0 +1,35 @@
+use core::fmt::Write;
+use crate::lib::vga::Writer;
+use crate::ColorCode;
+use crate::Color;
+
+pub struct Init<'a>{
+    pub initname: &'a str,
+    pub buffer: Writer,
+}
+
+impl<'a> Init<'a> {
+    pub fn new(position: (usize, usize), initname: &'static str) -> Self {
+        let buffer = Writer::from_position(position);
+        Self {
+            initname,
+            buffer
+        }
+    }
+    pub fn pending(&mut self) {
+        self.buffer.color_code = ColorCode::new(Color::White, Color::Black);
+        write!(self.buffer, "[..] {}", &self.initname).expect("Error");
+    }
+    pub fn ok(&mut self) {
+        self.buffer._clear_row(self.buffer.row_position);
+        self.buffer.reset_cursor();
+        self.buffer.color_code = ColorCode::new(Color::Green, Color::Black);
+        write!(self.buffer, "[OK] {}", &self.initname).expect("Error");
+    }
+    pub fn error(&mut self) {
+        self.buffer._clear_row(self.buffer.row_position);
+        self.buffer.reset_cursor();
+        self.buffer.color_code = ColorCode::new(Color::Red, Color::Black);
+        write!(self.buffer, "[ERR] {}", &self.initname).expect("Error");
+    }
+}

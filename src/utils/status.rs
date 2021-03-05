f5c9cfa7ba7608966c1cd55c77621467b7258f57
Rheydskey <matgag02@gmail.com>
2021-03-05 22:55:37 +0100

    kernel: Update Status

--- src/utils/status.rs
+++ src/utils/status.rs
@@ -15,18 +15,18 @@ impl<'a> Init<'a> {
     }
     pub fn pending(&mut self) {
         self.buffer.color_code = ColorCode::new(Color::White, Color::Black);
-        write!(self.buffer, "[..] {}", &self.initname).expect("Error");
+        write!(self.buffer, "[ .. ] {}", &self.initname).expect("Error");
     }
     pub fn ok(&mut self) {
         self.buffer._clear_row(self.buffer.row_position);
         self.buffer.reset_cursor();
         self.buffer.color_code = ColorCode::new(Color::Green, Color::Black);
-        write!(self.buffer, "[OK] {}", &self.initname).expect("Error");
+        write!(self.buffer, "[ OK ] {}", &self.initname).expect("Error");
     }
     pub fn error(&mut self) {
         self.buffer._clear_row(self.buffer.row_position);
         self.buffer.reset_cursor();
         self.buffer.color_code = ColorCode::new(Color::Red, Color::Black);
-        write!(self.buffer, "[ERR] {}", &self.initname).expect("Error");
+        write!(self.buffer, "[ ERR ] {}", &self.initname).expect("Error");
     }
 }

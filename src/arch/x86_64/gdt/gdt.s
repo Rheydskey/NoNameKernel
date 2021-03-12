aa4a0b867169a2d669294e98c5894b10ad330496
Rheydskey <matgag02@gmail.com>
2021-03-13 00:14:24 +0100

    kernel: Update GDT

--- src/arch/x86_64/gdt/gdt.s
+++ src/arch/x86_64/gdt/gdt.s
@@ -2,18 +2,25 @@
 
 global _x86_64_lgdt
 _x86_64_lgdt:
+  lgdt [rdi]
   push rbp
   mov rbp, rsp
-  lgdt [rdi]
-  mov ax, 0x10
-  mov ss, ax
-  mov ds, ax
-  mov es, ax
-  mov rax, qword .trampoline
-  push rsi
-  push rax
-  o64 retf
+
+  push qword 0x10
+  push rbp
+  pushf
+  push qword 0x8
+  push .trampoline
+  iretq
 
 .trampoline:
   pop rbp
+
+  mov ax, 0x10
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov ss, ax
+
   ret

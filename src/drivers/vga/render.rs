779bdbcaa4e8a110a188c65ed30fee7ba58c0d07
Rheydskey <matgag02@gmail.com>
2021-04-10 18:24:40 +0200

    Add print_color

--- src/drivers/vga/render.rs
+++ src/drivers/vga/render.rs
@@ -1,21 +1,47 @@
 use crate::drivers::vga::buffer::Writer;
-use core::fmt::{self, Write};
-use core::lazy::OnceCell;
+use core::{fmt::{self, Write}, lazy::OnceCell};
 
-pub static mut RENDER: OnceCell<Writer> = OnceCell::new();
+use super::vga_color::ColorCode;
+
+pub static mut BUFFER: core::lazy::OnceCell<Writer> = OnceCell::new();
 
 #[doc(hidden)]
 pub fn _print(args: fmt::Arguments) {
     unsafe {
-        if let Some(e) = RENDER.get_mut() {
+
+        BUFFER.get_or_init(|| Writer::default());
+
+        if let Some(e) = BUFFER.get_mut() {
             e.write_fmt(args).expect("Error");
         }
     }
 }
 
+#[doc(hidden)]
+pub fn _print_color(args: fmt::Arguments, color: ColorCode) {
+    unsafe {
+        BUFFER.get_or_init(|| Writer::default());
+
+        if let Some(e) = BUFFER.get_mut() {
+            e.color_code = color;
+            e.write_fmt(args).expect("Error");
+            e.color_code = ColorCode::new(super::vga_color::Color::White, super::vga_color::Color::Black);
+        } else {
+            panic!("Can't get Buffer");
+        }
+    }
+}
+
+
+
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::drivers::vga::render::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! print_color {
+    ($color:expr, $($arg:tt)*) => ($crate::drivers::vga::render::_print_color(format_args!($($arg)*), $color));
 }
 
 #[macro_export]
@@ -25,22 +51,7 @@ macro_rules! println {
 }
 
 #[macro_export]
-macro_rules! dbg {
-    () => {
-        $crate::println!("[{}:{}]", $crate::file!(), $crate::line!());
-    };
-    ($val:expr $(,)?) => {
-        // Use of `match` here is intentional because it affects the lifetimes
-        // of temporaries - https://stackoverflow.com/a/48732525/1063961
-        match $val {
-            tmp => {
-                $crate::println!("[{}:{}] {} = {:#?}",
-                    core::file!(), core::line!(), core::stringify!($val), &tmp);
-                tmp
-            }
-        }
-    };
-    ($($val:expr),+ $(,)?) => {
-        ($($crate::dbg!($val)),+,)
-    };
+macro_rules! println_color {
+    () => ($crate::println!());
+    ($color:expr, $($arg:tt)*) => ($crate::print_color!($color, "{}\n", format_args!($($arg)*)));
 }
--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,19 @@
 #![feature(const_maybe_uninit_assume_init)]
 #![feature(once_cell)]
 
+use drivers::vga::vga_color::{Color, ColorCode};
+
 mod arch;
 mod drivers;
 mod lib;
 mod utils;
 
-use crate::drivers::vga::buffer::Writer;
-use crate::drivers::vga::vga_color::{Color, ColorCode};
-use core::fmt::Write;
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut buffer = Writer::default();
-    buffer.color_code = ColorCode::new(Color::LightCyan, Color::Black);
-    write!(
-        buffer,
-        "Welcome on NoName Kernel {}-{}",
-        env!("CARGO_PKG_VERSION"),
-        env!("GIT_HASH")
-    )
-    .unwrap();
 
-    print!("Tetet");
+    println_color!(ColorCode::new(Color::LightCyan,Color::Black), "Welcome on NoName Kernel {}-{}", env!("CARGO_PKG_VERSION"), env!("GIT_HASH"));
+
+    print!("New features soon :)");
 
     loop {}
 }

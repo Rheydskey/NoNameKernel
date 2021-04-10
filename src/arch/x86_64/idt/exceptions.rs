8fe0b0deebec7c43b6a22bb1e5e8549954c3fd20
Rheydskey <matgag02@gmail.com>
2021-04-10 23:44:29 +0200

    Add IDT

---
+++ src/arch/x86_64/idt/exceptions.rs
@@ -0,0 +1,37 @@
+use super::idt::InterruptStackFrame;
+use crate::println;
+use super::idt::disable_interrupts;
+#[macro_export]
+macro_rules! interrupt {
+    ($arg:tt) => {
+        pub extern "x86-interrupt" fn $arg(interrupt: InterruptStackFrame) {
+            println!("Error : {:?}", interrupt);
+            unsafe {disable_interrupts()};
+        }
+    };
+}
+
+interrupt!(divide_by_zero);
+interrupt!(debug);
+interrupt!(non_maskable);
+interrupt!(breakpoint);
+interrupt!(overflow);
+interrupt!(bound_range);
+interrupt!(invalid_opcode);
+interrupt!(device_not_available);
+interrupt!(double_fault);
+
+interrupt!(invalid_tss);
+interrupt!(segment_not_present);
+interrupt!(stack_segment);
+interrupt!(protection);
+
+interrupt!(page_fault);
+
+interrupt!(fpu_fault);
+interrupt!(alignment_check);
+interrupt!(machine_check);
+interrupt!(simd);
+interrupt!(virtualization);
+interrupt!(security);
+

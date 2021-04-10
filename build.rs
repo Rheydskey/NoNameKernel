5724fe4e7ce6e2e6c2e05a744d62215f4ae10f46
Rheydskey <matgag02@gmail.com>
2021-04-10 19:14:07 +0200

    Add GDT

--- build.rs
+++ build.rs
@@ -1,10 +1,24 @@
 use std::process::Command;
+
 fn main() {
     // note: add error checking yourself.
     let output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
         .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap().split_at(7).0.to_string();
+    let git_hash = String::from_utf8(output.stdout)
+        .unwrap()
+        .split_at(7)
+        .0
+        .to_string();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+
+    nasm_rs::Build::new()
+        .target("x86_64-none-none")
+        .file("src/arch/x86_64/gdt/gdt.asm")
+        .flag("-F dwarf")
+        .flag("-w+all")
+        .flag("-Werror")
+        .compile("x86_64_gdt")
+        .expect("Error");
 }

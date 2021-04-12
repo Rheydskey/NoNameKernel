9cd59af4a6a6891bcf99968d09748d294bf339b9
Rheydskey <matgag02@gmail.com>
2021-04-12 15:41:10 +0200

    Improve way to handle keys

--- src/drivers/keyboard/mod.rs
+++ src/drivers/keyboard/mod.rs
@@ -1,14 +1,36 @@
 static KEYBOARDMAP: [char; 56] = [
-'\0','\0','1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\t',
-'q', 'w', 'e', 'r','t','y','u', 'i', 'o', 'p', '[', ']', '\n', '\0',
-'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'','`', ' ', '\\','z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', ' ', '*',   ];
+    '\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0', '\t', 'q', 'w',
+    'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', '\0', 'a', 's', 'd', 'f', 'g', 'h',
+    'j', 'k', 'l', ';', '\'', '`', ' ', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
+    ' ', '*',
+];
 
-fn translate(scancode: u8) -> char {
+enum Keyboard {
+    Key(char),
+    Esc,
+    Space,
+    Back,
+    Shift,
+    Ctrl,
+    Enter,
+    Other,
+}
 
-    if scancode >= KEYBOARDMAP.len() as u8{
-        '\0'
-    } else {
-        KEYBOARDMAP[scancode as usize]
+fn translate(scancode: u8) -> Keyboard {
+    match scancode {
+        1 => Keyboard::Esc,
+        14 => Keyboard::Back,
+        28 => Keyboard::Enter,
+        54 | 42 => Keyboard::Shift,
+        57 => Keyboard::Space,
+        157 | 224  => Keyboard::Ctrl,
+        _ => {
+            if scancode >= KEYBOARDMAP.len() as u8 {
+                Keyboard::Other
+            } else {
+                Keyboard::Key(KEYBOARDMAP[scancode as usize])
+            }
+        }
     }
 }
 
@@ -17,7 +39,16 @@ use crate::print;
 pub fn key_handler(scancode: u8) {
     let translate = translate(scancode);
 
-    if translate != '\0' {
-        print!("{}", translate);
+    match translate {
+        Keyboard::Key(e) => {
+            print!("{}", e)
+        }
+        Keyboard::Esc => {print!("ESC")}
+        Keyboard::Space => {print!(" ")}
+        Keyboard::Back => {print!("BACK")}
+        Keyboard::Shift => {print!("SHIFT")}
+        Keyboard::Ctrl => {print!("CTRL")}
+        Keyboard::Other => {}
+        Keyboard::Enter => {print!("\n")}
     }
 }

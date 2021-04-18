51487d94f687dba4377ac6616685207e7af20a78
Rheydskey <matgag02@gmail.com>
2021-04-18 22:37:25 +0200

    Update Status function

--- src/utils/status.rs
+++ src/utils/status.rs
@@ -2,6 +2,7 @@ use crate::drivers::vga::buffer::Writer;
 use crate::Color;
 use crate::ColorCode;
 use core::fmt::Write;
+use crate::drivers::vga::render::BUFFER;
 
 pub enum Status {
     UNKNOW,
@@ -10,7 +11,7 @@ pub enum Status {
     OK,
 }
 pub struct Init<'a> {
-    pub buffer: Writer,
+    pub position: (usize, usize),
     pub status: Status,
     pub initname: &'a str,
 }
@@ -19,48 +20,54 @@ impl<'a> Init<'a> {
     pub fn new(initname: &'static str) -> Self {
         let buffer = unsafe { crate::drivers::vga::render::BUFFER.get_mut().unwrap() };
 
-        let position = buffer._get_position();
-
         buffer.new_line();
 
-        let buffer = Writer::_from_position(position);
+        let position = buffer.get_position();
 
         Self {
-            buffer,
+            position,
             status: Status::UNKNOW,
             initname,
         }
     }
     pub fn pending(&mut self) {
+        let buffer = if let Some(e) = unsafe {BUFFER.get_mut()} {e} else {return;};
+
         match self.status {
             Status::PENDING => return,
             _ => (),
         }
-        self.buffer.color_code = ColorCode::new(Color::White, Color::Black);
+        
+        buffer.color_code = ColorCode::new(Color::White, Color::Black);
         self.status = Status::PENDING;
-        write!(self.buffer, "[ .. ] {}", &self.initname).expect("Error");
+        write!(buffer, "[ .. ] {}", &self.initname).expect("Error");
     }
     pub fn ok(&mut self) {
+        let buffer = if let Some(e) = unsafe {BUFFER.get_mut()} {e} else {return;};
+
         match self.status {
             Status::OK => return,
             _ => (),
         }
-        self.buffer.clear_row(self.buffer.row_position);
-        self.buffer.reset_cursor();
-        self.buffer.color_code = ColorCode::new(Color::Green, Color::Black);
+        buffer.set_position(self.position);
+        buffer.clear_row(self.position.0);
+        buffer.color_code = ColorCode::new(Color::Green, Color::Black);
         self.status = Status::OK;
-        write!(self.buffer, "[ OK ] {}", &self.initname).expect("Error");
+        write!(buffer, "[ OK ] {}", &self.initname).expect("Error");
     }
     pub fn _error(&mut self) {
+        let buffer = if let Some(e) = unsafe {BUFFER.get_mut()} {e} else {return;};
+
         match self.status {
             Status::ERROR => return,
             _ => (),
         }
-        self.buffer.clear_row(self.buffer.row_position);
-        self.buffer.reset_cursor();
-        self.buffer.color_code = ColorCode::new(Color::Red, Color::Black);
+
+        buffer.set_position(self.position);
+        buffer.clear_row(self.position.0);
+        buffer.color_code = ColorCode::new(Color::Red, Color::Black);
         self.status = Status::ERROR;
-        write!(self.buffer, "[ ERR ] {}", &self.initname).expect("Error");
+        write!(buffer, "[ ERR ] {}", &self.initname).expect("Error");
     }
 
     pub fn wait<F>(&mut self, callable: F) where F: FnOnce() -> bool {

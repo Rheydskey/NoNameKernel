b65b5f39f7a7ffc11fc1c642d22917fdeecd6f8c
Rheydskey <matgag02@gmail.com>
2021-05-02 09:17:19 +0200

    Move _start function to main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,28 +13,49 @@
 #![feature(destructuring_assignment)]
 #![allow(dead_code)]
 
-
-use arch::x86_64::{gdt::gdt_init, idt::init_idt};
-use drivers::vga::vga_color::{Color, ColorCode};
-
 mod arch;
 mod bootloader;
 mod drivers;
 mod lib;
 mod utils;
+mod kernel;
 
-#[cfg(feature = "stivale")]
-static STACK: [u8; 4096] = [0;4096];
+use crate::bootloader::BUFFER_ADDR;
+#[cfg(feature = "stivale2")]
+use bootloader::stivale2_header::{Stivale2Struct, Stivale2StructInner};
+
+use drivers::vga::{buffer::Writer, render::BUFFER};
+use kernel::kmain;
 
 
-pub fn kmain() {
-    println_color!(
-        ColorCode::new(Color::LightCyan, Color::Black),
-        "Welcome on NoName Kernel {}-{}",
-        env!("CARGO_PKG_VERSION"),
-        env!("GIT_HASH")
-    );
+#[cfg(feature = "bootimage")]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    unsafe {BUFFER_ADDR = 0xb8000};
+    unsafe { BUFFER.get_or_init(|| Writer::new_with_addr(0xb8000)) };
+    kmain();
+    loop {}
+}
 
-    utils::status::Init::new("GDT").wait(gdt_init);
-    utils::status::Init::new("IDT").wait(init_idt);
+#[cfg(feature = "stivale2")]
+pub unsafe fn load(address: usize) -> Stivale2Struct {
+    let inner = &*(address as *const Stivale2StructInner);
+    Stivale2Struct { inner }
+}
+
+#[cfg(feature = "stivale2")]
+#[no_mangle]
+pub extern "C" fn _start(stivale: usize) -> ! {
+    //let stivalestruct  = unsafe {load(stivale)};
+
+    /*let addr = stivalestruct.get_framebuffer();
+
+    if let Some(e) = addr {
+        unsafe { BUFFER.get_or_init(|| Writer::new_with_addr(e.framebuffer_addr as u8)) };
+    } else {
+        loop {}
+    }
+
+    //kmain();*/
+    loop {}
 }

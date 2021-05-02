f39591019fe4eed9403bba433afead6a2d298020
Rheydskey <matgag02@gmail.com>
2021-05-02 09:15:53 +0200

    bootloader: Update file

--- src/bootloader/mod.rs
+++ src/bootloader/mod.rs
@@ -1,28 +1,22 @@
 pub static mut BUFFER_ADDR: u64 = 0;
 
-#[cfg(feature = "stivale")]
-pub mod stivale;
-
 #[cfg(feature = "stivale2")]
-pub mod stivale2;
+pub mod stivale2_header;
 
-pub mod start {
+#[cfg(feature = "grub")]
+pub mod grub;
+
     #[cfg(feature = "stivale2")]
     pub mod stivale2 {
-        use crate::bootloader::stivale2::{Stivale2Header, Stivale2Struct};
-        use crate::{STACK, kmain};
-        use crate::bootloader::BUFFER_ADDR;
+        use crate::{bootloader::stivale2_header::{Stivale2Header, Stivale2Struct}, kernel::kmain};
+        use super::stivale2_header::Stivale2HeaderTagFramebuffer;
+
+        static STACK: [u8; 4096] = [0; 4096];
+        static FRAMEBUFFER_TAG: Stivale2HeaderTagFramebuffer = Stivale2HeaderTagFramebuffer::new();
 
         #[used]
         #[link_section = ".stivale2hdr"]
-        static STIVALE2: Stivale2Header = Stivale2Header::new(STACK[0] as *const u8);
-
-        #[no_mangle]
-        pub fn _start(stivale: &Stivale2Struct) -> ! {
-            crate::print!("{:?}", stivale.bootloader_brand);
-            kmain();
-            loop {}
-        }
+        static STIVALE2: Stivale2Header = Stivale2Header::new(STACK[0] as *const u8).set_tags((&FRAMEBUFFER_TAG as *const Stivale2HeaderTagFramebuffer).cast());
     }
 
     #[cfg(feature = "stivale")]
@@ -36,7 +30,7 @@ pub mod start {
         static STIVALEHDR: StivaleHeader = StivaleHeader::new(&STACK[0] as *const u8);
 
         #[no_mangle]
-        pub fn _start(stivale: &StivaleStruct) -> ! {
+        pub extern "C" fn _start(stivale: &StivaleStruct) -> ! {
             unsafe {BUFFER_ADDR = stivale.framebuffer_addr};
             unsafe {BUFFER.get_or_init(|| Writer::new_with_addr(unsafe {BUFFER_ADDR}))};
             let buf = unsafe {BUFFER.get_mut().unwrap()};
@@ -47,19 +41,29 @@ pub mod start {
             loop {}
         }
     }
-    #[cfg(feature = "bootimage")]
-    mod bootimage {
+
+    #[cfg(feature = "grub")]
+    mod grub {
         use crate::kmain;
         use crate::drivers::vga::buffer::Writer;
         use crate::drivers::vga::render::BUFFER;
         use crate::bootloader::BUFFER_ADDR;
+        use crate::bootloader::grub::{MultibootHeader, MULTIBOOT_HEADER_ARCHITECTURE_I386, MULTIBOOT2_HEADER_MAGIC};
+        use core::mem::size_of;
+
+        #[link_section = ".multiboot"]
+        static HEADER: MultibootHeader = MultibootHeader {
+            magic: MULTIBOOT2_HEADER_MAGIC,
+            architecture: MULTIBOOT_HEADER_ARCHITECTURE_I386,
+            header_lenght:  size_of::<MultibootHeader>() as u32,
+            checksum: (u32::MAX - (0xe85250d6 + MULTIBOOT_HEADER_ARCHITECTURE_I386 + size_of::<MultibootHeader>() as u32)) as u32
+        };
 
         #[no_mangle]
         pub fn _start() -> ! {
-            unsafe  {BUFFER_ADDR = 0xb8000};
-            unsafe {BUFFER.get_or_init(|| Writer::new_with_addr(0xb8000))};
+            unsafe { BUFFER_ADDR = 0xb8000 };
+            unsafe { BUFFER.get_or_init(|| Writer::new_with_addr(0xb8000)) };
             kmain();
             loop {}
         }
     }
-}
